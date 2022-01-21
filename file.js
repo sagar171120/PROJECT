4a7bc89eb8a0c82bc5667debcbb950e343470d17
sagar171120 <98139877+sagar171120@users.noreply.github.com>
2022-01-21 23:01:06 +0530

    Create file.js

---
+++ file.js
@@ -0,0 +1,209 @@
+//*[@id="top"]
+
+baseURI: "https://ashy-forest-0978d2910.azurestaticapps.net/index.html#top"
+childNodes: NodeList []
+data: " ==============header menu section starts here============== "
+firstChild: null
+isConnected: true
+lastChild: null
+length: 61
+nextElementSibling: section#header_menu.header_menu
+nextSibling: text
+nodeName: "#comment"
+nodeType: 8
+nodeValue: " ==============header menu section starts here============== "
+ownerDocument: document
+jQuery3510153968282089701921: {events: {…}, focusout: 1, focusin: 1, handle: ƒ}
+location: Location {ancestorOrigins: DOMStringList, href: 'https://ashy-forest-0978d2910.azurestaticapps.net/index.html#top', origin: 'https://ashy-forest-0978d2910.azurestaticapps.net', protocol: 'https:', host: 'ashy-forest-0978d2910.azurestaticapps.net', …}
+URL: "https://ashy-forest-0978d2910.azurestaticapps.net/index.html#top"
+activeElement: button.navbar-toggler.collapsed
+adoptedStyleSheets: []
+alinkColor: ""
+all: HTMLAllCollection(275) [html, head, meta, meta, link, link, link, title, body#top, section#header_menu.header_menu, div.container-fluid.px-0.shadow, nav.navbar.navbar-expand-lg.navbar-light.py-3, a.navbar-brand.pl-5.pl-small-0, img.img.img-fluid, button.navbar-toggler.collapsed, span.navbar-toggler-icon, div#navbarSupportedContent.navbar-collapse.collapse, ul.navbar-nav.mx-auto, li.nav-item.active, a.nav-link, span.sr-only, li.nav-item, a.nav-link, li.nav-item, a.nav-link, li.nav-item, a.nav-link, li.nav-item, a.nav-link, div.search.mr-3, a.btn.btn-sm.btn-outline-primary, div.cart, a, i.fas.fa-shopping-cart.fa-2x, span.badge.badge-pill.badge-primary, section#header_carousel.header_carousel, div#carouselExampleControls.carousel.slide, div.carousel-inner, div.carousel-item.active, img.d-block.w-100, div.carousel-item, img.d-block.w-100, div.carousel-item, img.d-block.w-100, a.carousel-control-prev, span.carousel-control-prev-icon, span.sr-only, a.carousel-control-next, span.carousel-control-next-icon, span.sr-only, section#shop_by_category.shop_by_category.py-5.my-5, div.container-fluid, div.section_title.text-center.mb-5, h1.text-capitalize, div.row, div.col-md-4.col-12, div.card.bg-dark.text-white, a, img.card-img.img.img-fluid, div.col-md-4.col-12.mt-small-5.mb-small-5, div.card.bg-dark.text-white, a, img.card-img.img.img-fluid, div.col-md-4.col-12, div.card.bg-dark.text-white, a, img.card-img.img.img-fluid, section#products.products.py-5.my-5, div.container, div.section_title.text-center.mb-5, h1.text-capitalize, div.row.mb-5, div.col-md-4.col-12, div.single_product.shadow.text-center.p-1, div.product_img, a, img.img.img-fluid, div.new_product, span.badge.py-2.px-3.badge-pill.badge-danger, div.product-caption.my-3, div.product-ratting, i.fas.fa-star, i.fas.fa-star, i.fas.fa-star, i.far.fa-star.low-star, i.far.fa-star.low-star, h4, a, div.price, b, span.mr-1, span, div.col-md-4.col-12, div.single_product.shadow.text-center.p-1.mt-small-5.mb-small-5, div.product_img, a, img.img.img-fluid, div.new_product, span.badge.py-2.px-3.badge-pill.badge-danger, div.product-caption.my-3, …]
+anchors: HTMLCollection []
+applets: HTMLCollection []
+baseURI: "https://ashy-forest-0978d2910.azurestaticapps.net/index.html#top"
+bgColor: ""
+body: body#top
+characterSet: "UTF-8"
+charset: "UTF-8"
+childElementCount: 1
+childNodes: NodeList(2) [<!DOCTYPE html>, html]
+children: HTMLCollection [html]
+compatMode: "CSS1Compat"
+contentType: "text/html"
+cookie: ""
+currentScript: null
+defaultView: Window {window: Window, self: Window, document: document, name: '', location: Location, …}
+designMode: "off"
+dir: ""
+doctype: <!DOCTYPE html>
+documentElement: html
+documentURI: "https://ashy-forest-0978d2910.azurestaticapps.net/index.html#top"
+domain: "ashy-forest-0978d2910.azurestaticapps.net"
+embeds: HTMLCollection []
+featurePolicy: FeaturePolicy {}
+fgColor: ""
+firstChild: <!DOCTYPE html>
+firstElementChild: html
+fonts: FontFaceSet {onloading: null, onloadingdone: null, onloadingerror: null, ready: Promise, status: 'loaded', …}
+forms: HTMLCollection []
+fragmentDirective: FragmentDirective {}
+fullscreen: false
+fullscreenElement: null
+fullscreenEnabled: true
+head: head
+hidden: false
+images: HTMLCollection(14) [img.img.img-fluid, img.d-block.w-100, img.d-block.w-100, img.d-block.w-100, img.card-img.img.img-fluid, img.card-img.img.img-fluid, img.card-img.img.img-fluid, img.img.img-fluid, img.img.img-fluid, img.img.img-fluid, img.img.img-fluid, img.img.img-fluid, img.img.img-fluid, img.img.img-fluid]
+implementation: DOMImplementation {}
+inputEncoding: "UTF-8"
+isConnected: true
+lastChild: html
+lastElementChild: html
+lastModified: "01/09/2022 19:17:59"
+linkColor: ""
+links: HTMLCollection(39) [a.navbar-brand.pl-5.pl-small-0, a.nav-link, a.nav-link, a.nav-link, a.nav-link, a.nav-link, a.btn.btn-sm.btn-outline-primary, a, a.carousel-control-prev, a.carousel-control-next, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a#button.btn.btn-lg.btn-outline-danger, button: a#button.btn.btn-lg.btn-outline-danger]
+nextSibling: null
+nodeName: "#document"
+nodeType: 9
+nodeValue: null
+onabort: null
+onanimationend: null
+onanimationiteration: null
+onanimationstart: null
+onauxclick: null
+onbeforecopy: null
+onbeforecut: null
+onbeforepaste: null
+onbeforexrselect: null
+onblur: null
+oncancel: null
+oncanplay: null
+oncanplaythrough: null
+onchange: null
+onclick: null
+onclose: null
+oncontextmenu: null
+oncopy: null
+oncuechange: null
+oncut: null
+ondblclick: null
+ondrag: null
+ondragend: null
+ondragenter: null
+ondragleave: null
+ondragover: null
+ondragstart: null
+ondrop: null
+ondurationchange: null
+onemptied: null
+onended: null
+onerror: null
+onfocus: null
+onformdata: null
+onfreeze: null
+onfullscreenchange: null
+onfullscreenerror: null
+ongotpointercapture: null
+oninput: null
+oninvalid: null
+onkeydown: null
+onkeypress: null
+onkeyup: null
+onload: null
+onloadeddata: null
+onloadedmetadata: null
+onloadstart: null
+onlostpointercapture: null
+onmousedown: null
+onmouseenter: null
+onmouseleave: null
+onmousemove: null
+onmouseout: null
+onmouseover: null
+onmouseup: null
+onmousewheel: null
+onpaste: null
+onpause: null
+onplay: null
+onplaying: null
+onpointercancel: null
+onpointerdown: null
+onpointerenter: null
+onpointerleave: null
+onpointerlockchange: null
+onpointerlockerror: null
+onpointermove: null
+onpointerout: null
+onpointerover: null
+onpointerrawupdate: null
+onpointerup: null
+onprogress: null
+onratechange: null
+onreadystatechange: null
+onreset: null
+onresize: null
+onresume: null
+onscroll: null
+onsearch: null
+onsecuritypolicyviolation: null
+onseeked: null
+onseeking: null
+onselect: null
+onselectionchange: null
+onselectstart: null
+onslotchange: null
+onstalled: null
+onsubmit: null
+onsuspend: null
+ontimeupdate: null
+ontoggle: null
+ontransitioncancel: null
+ontransitionend: null
+ontransitionrun: null
+ontransitionstart: null
+onvisibilitychange: null
+onvolumechange: null
+onwaiting: null
+onwebkitanimationend: null
+onwebkitanimationiteration: null
+onwebkitanimationstart: null
+onwebkitfullscreenchange: null
+onwebkitfullscreenerror: null
+onwebkittransitionend: null
+onwheel: null
+ownerDocument: null
+parentElement: null
+parentNode: null
+pictureInPictureElement: null
+pictureInPictureEnabled: true
+plugins: HTMLCollection []
+pointerLockElement: null
+previousSibling: null
+readyState: "complete"
+referrer: ""
+rootElement: null
+scripts: HTMLCollection(3) [script, script, script]
+scrollingElement: html
+styleSheets: StyleSheetList {0: CSSStyleSheet, 1: CSSStyleSheet, 2: CSSStyleSheet, length: 3}
+textContent: null
+timeline: DocumentTimeline {currentTime: 1805081.9}
+title: "E-Commerce"
+visibilityState: "visible"
+vlinkColor: ""
+wasDiscarded: false
+webkitCurrentFullScreenElement: null
+webkitFullscreenElement: null
+webkitFullscreenEnabled: true
+webkitHidden: false
+webkitIsFullScreen: false
+webkitVisibilityState: "visible"
+xmlEncoding: null
+xmlStandalone: false
+xmlVersion: null
+[[Prototype]]: HTMLDocument
+parentElement: body#top
+parentNode: body#top
+previousElementSibling: null
+previousSibling: text
+textContent: " ==============header menu section starts here============== "
